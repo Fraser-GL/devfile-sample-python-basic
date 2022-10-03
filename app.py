30b43c611a41b29ddf8624c3fc3b5984802ef91f
fraser <fraser.brown@ecs.co.uk>
2022-10-03 15:48:24 +0100

    adding db connection

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+rom flask import Flask
 import os
+import pymongo 
 
 app = Flask(__name__)
 
@@ -7,8 +8,27 @@ app = Flask(__name__)
 def hello():
     return "Hello World! I have updated this code"
 
+def get_database():
+    CONNECTION_STRING = "mongodb.fraser-brown-dev.svc.cluster.local"
+    client = pymongo.MongoClient(CONNECTION_STRING)
+ 
+    db = client.testTable
+
+    people = db.people
+    personDocument = {
+        "name": { "first": "Alan", "last": "Turing" },
+        "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+        "views": 1250000
+    } 
+    people.insert_one(personDocument)
+    return client['testTable']
+  
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
+    dbname = get_database()
 
     app.run(port=port,host='0.0.0.0')
+
+
